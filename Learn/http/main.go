e0195e8b781bd114fe45479a202b85e3a29c387b
Chantiraj07 <chantiraj07@gmail.com>
2026-01-13 14:06:33 -0500

    Channels

--- Learn/http/main.go
+++ Learn/http/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+//custom writer
+
+type logWriter struct {
+}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -12,5 +18,21 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	//fmt.Println(resp)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	//other way
+	//io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+	//custom writer
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
